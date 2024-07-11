d3caad28b83710f4c8cde39e7d6adf84e9b646e0
SEYYED UVAIS <156394395+Ucode-uvais@users.noreply.github.com>
2024-07-11 06:39:03 +0530

    Update server.js

--- weather-backend/server.js
+++ weather-backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("../index.html"));
+app.use(express.static(__dirname));
 
 app.get("/weather", async (req, res) => {
   const { city } = req.query;
